b52bfe11815afcedf05ece483b86c8dfd3dddc84
ayaomar123 <44114766+ayaomar123@users.noreply.github.com>
2020-07-08 16:34:52 +0300

    first-commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include 'person.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	 
+	<?php 
+
+		$pet01 = new Pet();
+		echo $pet01-> owner();
+	?> 
+
+</body>
+</html> 
